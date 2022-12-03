cc60b53ea47d0e34d4517eb07eac564aa26f79d1
peterlim995 <peterlim995@gmail.com>
2022-12-03 15:48:04 -0500

    complete the javascript code / no error found

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,55 +23,139 @@ $(function () {
 });
 
 
+var currentDayEl = $('#currentDay');
+var containerEl = $('#container');
+var begin = 9; // begining time of work is 9 am 
+var end = 17; // ending time of work is 5 pm
+var total = end-begin+1; // total working hour : 9 hours
+
+// console.log("Begin");
+
 // display current time
 function displaytime(){
-
-
+  var showDate = dayjs().format('dddd, MMMM D ');
+  currentDayEl.text(showDate);   
 }
 
-// Read schedule from local storage and returns array of schedule object
+// Read schedule from local storage and returns array of schedule
+// Returns an array that has no content inside if there aren't any schedule.
 function readScheduleFromStorage(){
-  var schedules = localStorage.getItem('schedules');
+  var schedules = JSON.parse(localStorage.getItem('schedules'));
 
+  if(schedules !== null){
+    // console.log("exist!");
+    return schedules;
+  }else {
+    // console.log("Not exist!")
+    schedules = [];
+    for (let i = 0; i < total; i++) {
+      schedules.push('');      
+    }    
+  }
   return schedules;
 }
 
 // Get schedule data from local storage and displays it.
 function printScheduleData(){
 
-  var schedule = readScheduleFromStorage();
+  containerEl.empty();
 
+  var schedules = readScheduleFromStorage();
+
+  var indexHour = begin; // begining time work
+  var totalWorkingHour = total; // total working hours
+
+  for (let i = 0; i < totalWorkingHour; i++) {
+    var schedule = schedules[i];
+    
+    // get hour of today
+    var currentHour = parseInt(dayjs().format('H'));
+    // console.log("Current Hour: "+currentHour);
+    
+    // if schedule hour is passed class is past
+    // if schedule hour is passed class is present
+    // if schedule hour is passed class is future
+    if(indexHour < currentHour){
+      var divRow = $('<div id="hour-'+ indexHour+ '" class="row time-block past">');
+    } else if(indexHour === currentHour){
+      var divRow = $('<div id="hour-'+ indexHour+ '" class="row time-block present">');
+    } else {
+      var divRow = $('<div id="hour-'+ indexHour+ '" class="row time-block future">');
+    }
+    
+    // If hour is less than 12 put AM
+    // If hour is 12PM
+    // If hour is more than 12 put PM
+    if(indexHour < 12){
+      var divHour = $('<div class="col-2 col-md-1 hour text-center py-3">'+ indexHour +'AM</div>');
+    } else if (indexHour === 12){
+      var divHour = $('<div class="col-2 col-md-1 hour text-center py-3">'+ indexHour +'PM</div>');
+    } else {
+      var divHour = $('<div class="col-2 col-md-1 hour text-center py-3">'+ (indexHour - 12) +'PM</div>');
+    }
+
+    // display schedule description
+    var textDescription = $('<textarea class="col-8 col-md-10 description" rows="3">'+ schedule +'</textarea>');
+    
+    // Save Btn
+    // Setting data-index attr as beginHour
+    var saveBtn = $('<button class="btn saveBtn col-2 col-md-1" aria-label="save" data-index="'+indexHour+'"><i class="fas fa-save" aria-hidden="true"></i></button>');
+
+     // append elements to DOM to display them
+    divRow.append(divHour, textDescription, saveBtn);
+    containerEl.append(divRow);
+
+    indexHour++;  // for next hour
+  }
 }
 
 
 // Take an array of schedules and saves them in localStorage.
 function saveScheduleToStorage(schedules){
-    localStorage.setItem('schedules', JSON.stringify(schedules));
+    localStorage.setItem('schedules',JSON.stringify(schedules));
 }
 
 // Adds a schedule to local storage and prints the schedule data
 function handleSaveSchedule(){
 
-  var newSchedule = {};
+  // Getting data-index attr from the button
+  var dataIndex = $(this).attr('data-index');
+  // console.log("dataIndex: "+dataIndex);
 
-  // add schedule to local storage
+  // find <div id="hour-(dataIndex)">
+  var textArea = containerEl.children('#hour-'+dataIndex);
+  // console.log("textArea: "+textArea);
+
+  // Getting textarea value
+  var description = textArea.children('textarea').val().trim();
+  // console.log("description: "+description);
+
+  // Getting old schedule from local storage
   var schedules = readScheduleFromStorage();
-  schedules.push(newSchedule);
+
+  // decide the updateing index
+  var updateIndex = dataIndex - begin;
+
+  // replace schedule description
+  schedules.splice(updateIndex,1,description);
+  
+  // save updated schedules to local storage
   saveScheduleToStorage(schedules);
 
-  // print schedule data
+  // print new schedule data
   printScheduleData();
 
 }
 
-// When click the save button 
-scheduleDisplayEl.on('click', '.btn-save-project', handleSaveSchedule);
 
 printScheduleData();
+// When click the save button 
+
+containerEl.on('click', '.btn', handleSaveSchedule);
 
 displaytime();
 
-
+setInterval(displaytime, 1000);
 
 
 
